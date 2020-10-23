780a72be23b801ac62fa0b9b74f1e74af9c0001e
YuliaNov <yulya.novichkova21@gmail.com>
2020-10-23 13:21:53 +0700

    Сделано задание 2.2.3.3 из 2 лабораторной

--- Lab_2/Structure.cpp
+++ Lab_2/Structure.cpp
@@ -3,65 +3,95 @@
 
 using namespace std;
 
+const int coutn = 3;
+
 void DemoRectangle() 
 { 
-	Regtangle regtangle;
-	cout << "Enter length regtangle (positive number):" << endl;
-	cin >> regtangle.Length;
-	cout << "Enter width regtangle (positive number):" << endl;
-	cin >> regtangle.Width;
-	cout << "Enter color regtangle:" << endl;
-	cin >> regtangle.Color;
+	Regtangle regtangle[coutn];
+	for (int i = 0; i < coutn; i++)
+	{
+		cout << "Enter length regtangle (positive number):" << endl;
+		cin >> regtangle[i].Length;
+		cout << "Enter width regtangle (positive number):" << endl;
+		cin >> regtangle[i].Width;
+		cout << "Enter color regtangle:" << endl;
+		cin >> regtangle[i].Color;
+		cout << endl;
+	}
 
-	cout << "The rectangle has dimensions: " 
-		<< regtangle.Length << "x" << regtangle.Width << endl;
+	for (int i = 1; i <= coutn; i++)
+	{
+		cout << i << ". " << "The rectangle has dimensions: "
+			<< regtangle[i].Length << "x" << regtangle[i].Width << endl;
+	}
 } 
 
 void DemoFlight() 
 {
-	Flight flight;
-	cout << "Enter departure flight:" << endl;
-	cin >> flight.Departure;
-	cout << "Enter destination flight:" << endl;
-	cin >> flight.Destination;
-	cout << "Enter time flight:" << endl;
-	cin >> flight.TimeFlight;
+	Flight flight[coutn];
+	for (int i = 0; i < coutn; i++)
+	{
+		cout << "Enter departure flight:" << endl;
+		cin >> flight[i].Departure;
+		cout << "Enter destination flight:" << endl;
+		cin >> flight[i].Destination;
+		cout << "Enter time flight:" << endl;
+		cin >> flight[i].TimeFlight;
+		cout << endl;
+	}
 
-	cout << "Flight " << flight.Departure << " - " << flight.Destination
-		<< " is on the way " << flight.TimeFlight << " minutes" << endl;
+	for (int i = 1; i <= coutn; i++)
+	{
+		cout << i << ". " << "Flight " << flight[i].Departure << " - " << flight[i].Destination
+			<< " is on the way " << flight[i].TimeFlight << " minutes" << endl;
+	}
 } 
 
 void DemoMovie() 
 {
-	Movie movie;
-	cout << "Enter title movie:" << endl;
-	cin >> movie.Title;
-	cout << "Enter duration movie:" << endl;
-	cin >> movie.Duration;
-	cout << "Enter year movie:" << endl;
-	cin >> movie.Year;
-	cout << "Enter genre movie:" << endl;
-	cin >> movie.Genre;
-	cout << "Enter raiting movie:" << endl;
-	cin >> movie.Raiting;
+	Movie movie[coutn];
+	for (int i = 0; i < coutn; i++)
+	{
+		cout << "Enter title movie:" << endl;
+		cin >> movie[i].Title;
+		cout << "Enter duration movie:" << endl;
+		cin >> movie[i].Duration;
+		cout << "Enter year movie:" << endl;
+		cin >> movie[i].Year;
+		cout << "Enter genre movie:" << endl;
+		cin >> movie[i].Genre;
+		cout << "Enter raiting movie:" << endl;
+		cin >> movie[i].Raiting;
+		cout << endl;
+	}
 
-	cout << "Title movie: " << movie.Title << endl
-		<< "Duration: " << movie.Duration << endl
-		<< "Year: " << movie.Year << endl
-		<< "Genre: " << movie.Genre << endl
-		<< "Raiting: " << movie.Raiting << endl;
+	for (int i = 1; i <= coutn; i++)
+	{
+		cout << i << ". " << "Title movie: " << movie[i].Title << endl
+			<< "Duration: " << movie[i].Duration << endl
+			<< "Year: " << movie[i].Year << endl
+			<< "Genre: " << movie[i].Genre << endl
+			<< "Raiting: " << movie[i].Raiting << endl;
+	}
 } 
 
 void DemoTime() 
 {
-	Time time;
-	cout << "Enter hours (from 0 to 23):" << endl;
-	cin >> time.Hours;
-	cout << "Enter minuts (from 0 to 60):" << endl;
-	cin >> time.Minuts;
-	cout << "Enter seconds (from 0 to 60):" << endl;
-	cin >> time.Seconds;
+	Time time[coutn];
+	for (int i = 0; i < coutn; i++)
+	{
+		cout << "Enter hours (from 0 to 23):" << endl;
+		cin >> time[i].Hours;
+		cout << "Enter minuts (from 0 to 60):" << endl;
+		cin >> time[i].Minuts;
+		cout << "Enter seconds (from 0 to 60):" << endl;
+		cin >> time[i].Seconds;
+		cout << endl;
+	}
 
-	cout << "Time " << time.Hours << ":" << time.Minuts << ":" 
-		<< time.Seconds << endl;
+	for (int i = 1; i <= coutn; i++)
+	{
+		cout << i << ". " << "Time " << time[i].Hours << ":" << time[i].Minuts << ":"
+			<< time[i].Seconds << endl;
+	}
 }
