844c1bac22f06a91a5c3ddf1beac4bd7cb1b33c2
YuliaNov <yulya.novichkova21@gmail.com>
2020-11-30 17:21:58 +0700

    Задание 5.1.1. для лабораторной работы 5.

---
+++ Lab_5/Person.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class Person
+{
+private:
+	string _name;
+	string _surname;
+	string _patronymic;
+
+public:
+	void SetName(string name);
+	void SetSurname(string surname);
+	void SetPatronymic(string patronymic);
+
+	string GetName();
+	string GetSurname();
+	string GetPatronymic();
+
+	Person();
+	Person(string name, string surname, string patronymic);
+};
